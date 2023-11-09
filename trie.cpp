8585bbd88525a5ea68052413dcd1c1351a45033a
bbarrett99 <bethanygracebarrett@gmail.com>
2023-11-09 09:53:53 -0700

    updates

--- trie.cpp
+++ trie.cpp
@@ -7,3 +7,56 @@
  */
 
 #include "trie.h"
+
+trie::trie() {
+    root = new node();
+}
+
+trie::~trie() {
+    destroy(root);
+}
+
+void trie::insert(const string &s) {
+    if (root->is_terminal) {
+        root->is_terminal = false;
+    }
+    node* currNode = root;
+    for (int i = 0; i < s.length() - 1; i++) {
+        if (currNode->children[(int)s[i] - 61] != nullptr) {
+            currNode = currNode->children[s[i] - 61];
+        }
+        else {
+            currNode->children[(int)s[i] - 61] = new node();
+            currNode = currNode->children[s[i] - 61];
+
+        }
+    }
+    if (currNode->children[(int)s[s.length() - 1] - 61]) {
+
+    }
+}
+
+bool trie::contains(const string &s) {
+    return true;
+}
+
+bool trie::is_prefix(const string &s) {
+    return true;
+}
+
+void trie::extend(const string &prefix, vector<string> &result) {
+    return;
+}
+
+void trie::destroy(node* root) {
+    if (root == nullptr) {
+        return;
+    }
+
+    if (!root->is_terminal) {
+        for (int i = 0; i < 26; i++) {
+                destroy(root->children[i]);
+        }
+    }
+    delete root;
+}
--- trie.h
+++ trie.h
@@ -16,13 +16,32 @@ using namespace std;
 
 class trie {
 public:
+    trie();
+    ~trie();
+    trie(const trie& OTHER) = delete;
+    trie& operator=(const trie& OTHER) = delete;
     void insert(const string &s);
     bool contains(const string &s);
     bool is_prefix(const string &s);
     void extend(const string &prefix, vector<string> &result);
 
 private:
+    class node {
+        public:
+            node() {
+                for (int i = 0; i < 26; i++) {
+                    children[i] = nullptr;
+                }
+                is_terminal = false;
+            }
+            bool is_terminal;
+            node* children[26];
 
+    };
+    node* root;
+    size_t size;
+
+    void destroy(node* root);
 };
 
 #endif
